85fc2495a9dbf41ec0590c236157f971a0c05c83
Aadeshh0 <ag9494@srmist.edu.in>
2024-08-02 20:25:09 +0530

    commit

--- bajaj_backend/api/index.py
+++ bajaj_backend/api/index.py
@@ -1,5 +1,37 @@
-from bajaj_backend.app import app
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-# This is necessary for Vercel serverless deployment
+app = Flask(__name__)
+CORS(app)
+
+USER_ID = "your_name"
+EMAIL = "cllg@email.id"
+ROLL_NUMBER = "RA_number"
+
+@app.route('/bfhl', methods = ['POST'])
+def process_data():
+    data = request.json.get('data', [])
+
+    if not isinstance(data, list):
+        return jsonify({"is_success": False, "message": "Invalid Input"}), 400
+
+    numbers = [item for item in data if item.isdigit()]
+    alphabets = [item for item in data if item.isalpha() and len(item) == 1]
+    highest_alphabet  = [max(alphabets, key=str.lower())] if alphabets else []
+
+    return jsonify({
+        "is_success" : True,
+        "user_id" : USER_ID,
+        "email" : EMAIL,
+        "roll_number" : ROLL_NUMBER,
+        "numbers" : numbers,
+        "alphabets" : alphabets,
+        "highest_alphabet" : highest_alphabet
+    })
+
+@app.route('/bfhl', methods=['GET'])
+def get_operation_code():
+    return jsonify({"operation code":1})
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
